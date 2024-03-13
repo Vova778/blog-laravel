2db8e5ba3fbb5270fe710fedfe235229b38aa281
Vova778 <voha.kopovay@gmail.com>
2024-03-14 00:08:53 +0200

    email notifications added

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,7 +10,7 @@ class Comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'comment', 'article_id'];
+    protected $fillable = ['user_id', 'comment', 'post_id'];
 
     public function user(): BelongsTo
     {
